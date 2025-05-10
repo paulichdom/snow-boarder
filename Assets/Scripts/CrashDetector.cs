2c1bb8c8a18f7730a85680dacbefe1c58e7d4852
paulichdom <paulic.domagoj@gmail.com>
2025-05-10 18:00:51 +0200

    delay scene reload

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -3,12 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] private float delayValue = 0.5f;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Ground"))
         {
-            SceneManager.LoadScene(0);
+            Invoke(nameof(ReloadScene), delayValue);
         }
         
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -2,12 +2,18 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] private float delayValue = 0.5f;
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneManager.LoadScene(0);
+            Invoke(nameof(ReloadScene), delayValue);
         }
         
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
